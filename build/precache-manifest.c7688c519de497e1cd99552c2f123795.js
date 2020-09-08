f2c4d232b06ce1f4a9c161d2f2f039fe53b41e08
bruno pessoa <brunopessoa.melo@gmail.com>
2020-09-08 20:27:05 -0300

    buildando o projeto.

---
+++ build/precache-manifest.c7688c519de497e1cd99552c2f123795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd680ea35f4d759b514ad60dc33d74b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002c27002d7c624c28b1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e609218678cd164bdcb4",
+    "url": "/static/js/2.70fdf36a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70fdf36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002c27002d7c624c28b1",
+    "url": "/static/js/main.80f7b1ec.chunk.js"
+  },
+  {
+    "revision": "2626befa39443feb5be3",
+    "url": "/static/js/runtime-main.0f605650.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
