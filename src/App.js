0957eb54ac96712299e3a9d456f779c48de322dc
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-18 22:24:11 -0300

    criando a lógica do Componente

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ import {Route, Switch} from "react-router-dom";
 function App(){
   return (
     <>
-
+    <Navbar />
     <Switch>
       <Route exact path="/" component={Home}/>
       <Route exact path="/rooms/" component={Rooms}/>
-      <Route exact path="/rooms/:slug" component={SingleRoom}/>
+      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Route component={Error} />
     </Switch>
     </>
   );
---
+++ src/context.js
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+import items from "./data";
+import Client from "./Contentful";
+
+const RoomContext = React.createContext();
+
+export default class RoomProvider extends Component{
+    state = {
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        laoding: true,
+        type: "all",
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        breakfast: false,
+        pets: false
+    };
+   /*  getData = async () =>{
+        try {
+            let response = await Client.getEntries({
+                content_type: "beachResortRoom"
+            });
+            let featuredRooms = rooms.fiter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featureadRooms,
+                sortedRooms,
+                loading: false,
+                //
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        } */
+
+        componentDidMount(){
+            // this.getData();
+            let rooms = this.formatData(items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            //
+
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featuredRooms,
+                loading: false,
+
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        }
+
+
+        formatData(items) {
+            let tempItems = items.map(item => {
+                let id = item.sys.id;
+                let images = item.fields.images.map(image => image.fields.file.url);
+
+                let room = {...item.fields, images, id};
+                return room;
+                });
+                return tempItems;
+             }
+        }
+
+        getRoom = slug => {
+            let tempRooms = [...this.state.rooms];
+            const room = tempoRooms.find(room => room.slug === slug);
+            return room;
+        };
+
+        handleChange = event =>{
+            const target = event.target;
+            const value = target.type === "checkbox" ? target.checked : target.value;
+            const name = target.name;
+            console.log(name, value);
+
+            this.setState(
+                {
+                    [name]: value
+                },
+                this.filterRooms
+            );
+        };
+
+        filterRooms = () => {
+            let{
+                rooms,
+                type,
+                capacity,
+                price,
+                minSize,
+                maxSize,
+                breakfast,
+                pets,
+            } = this.state;
+
+            let tempoRooms = [...rooms];
+
+            //transformar os valores 
+            // pega os capacity
+            capacity = parsetInt(capacity);
+            price = parseInt(price);
+
+            //filtrado pelo tipo, filtrar os quartos pelo tipo
+            if(type !== "all"){
+                tempoRooms = tempoRooms.filter(room => room.type === type);
+            }
+
+            //filtrar pelo capacity, irá filtrar os Quartos com maior capacidade 
+            if(capacity !== 1){
+                tempoRooms = tempoRooms.filter(room => room.capacity >= capacity);
+            }
+
+            // filtrar os quartos pelo preço 
+            tempoRooms = tempoRooms.filter(room => room.price <= price);
+
+            //filtrar pelo quartos pelo tamanho
+            tempoRooms = tempoRooms.filter( room => room.size >= minSize && room.size <= maxSize);
+
+            //filtrar os cafés da manhã
+
+            if(breakfast){
+                tempoRooms = tempoRooms.filter(room => room.breakfast === true);
+            }
+
+            //filtrar quartos que oferecem vaga para pets
+
+            if(pets){
+                tempoRooms = tempoRooms.filter(room => room.pets === true);
+            }
+
+            this.setState({
+                sortedRooms: tempoRooms
+            });
+        };
+
+        render() {
+            return (
+              <RoomContext.Provider
+                value={{
+                  ...this.state,
+                  getRoom: this.getRoom,
+                  handleChange: this.handleChange
+                }}
+              >
+                {this.props.children}
+              </RoomContext.Provider>
+            );
+          }
+        }
+
+        const RoomConsumer = RoomContext.Consumer;
+
+        export {RoomProvider, RoomConsumer, RoomContext};
+
+        export function withRoomConsumer(Component){
+            return function ConsumerWrapper(props){
+                return(
+                    <RoomConsumer>
+                        {value => <Component {...props} context={value} />}
+                    </RoomConsumer>
+                );
+            };
+        }
+    
